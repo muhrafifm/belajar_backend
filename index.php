e0f0fa7156c9dd3140315a868a597b32ce93a26f
muhrafifm <muhrafif52@gmail.com>
2021-04-04 21:03:50 -0700

    coding pertama

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	</body>
+		<title>Komputer Sederhana</title>
+	</head>
+	<body>
+		<form action="proses.php" method="post">
+			<input type="text" name="angka1">
+			<input type="text" name="angka2">
+			<input type="submit" name=tambah"value="tambah">
+		</form>
+
+
+</body>
+</html>
+<html>
+	
